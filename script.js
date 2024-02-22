93e8cfc90ddbacd10378f85d33d600b1dbe52e0e
Abdul-Rasith <157093303+Abdul-Rasith@users.noreply.github.com>
2024-02-22 13:26:53 +0530

    this is small dom related task

---
+++ script.js
@@ -0,0 +1,15 @@
+var span = document.createElement("span");
+span.innerHTML = "This is span"
+var break1 = document.createElement("br")
+// 
+ var div = document.createElement("div");
+ div.innerHTML = "this is div"
+
+document.body.append(span,break1,div)
+
+
+    var input = prompt("Enter any suggesion")
+    var span1 = document.createElement("span");
+    span1.innerHTML = input
+    document.body.append(span1) 
+
